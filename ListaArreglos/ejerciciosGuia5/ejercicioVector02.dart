d72908cbdfe130898b16efcd7f9ccd380664afc5
unknown <SENA@MANAPRCPIFSD001.FormacionSena.RED>
2024-05-23 08:26:09 -0500

    se hicieron ejercicios de vectores

---
+++ ListaArreglos/ejerciciosGuia5/ejercicioVector02.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  //David Andres Morales - eje vector 2
+  /*
+  Llenar  dos  vectores  A  y  B  de  10  elementos  cada  uno,  sumar  el  elemento  uno  del  vector  A  con  el 
+  elemento uno del vector B y así sucesivamente hasta 10, almacenar el resultado en un vector C, e imprimir el vector resultante.
+  */
+  //Definiciones Variables
+  List<double> numeroA = [];
+  List<double> numeroB = [];
+  List<double> numerosc = [];
+  int cantNumerosA = 10;
+}
