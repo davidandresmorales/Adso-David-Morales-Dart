27dbf86d573dbc45d5b393ded2e3ab260dfe9163
davidandresmorales <dmoralesguapacha684@gmail.com>
2024-06-06 00:11:11 -0500

    se hicieron ejercicios de vectores

--- ListaArreglos/ejerciciosGuia5/ejercicioVector09.dart
+++ ListaArreglos/ejerciciosGuia5/ejercicioVector09.dart
@@ -1,3 +1,47 @@
-void main(List<String> args) {
-  
+import 'dart:io';
+
+void main() {
+  // Definición Variables
+  List<String> nombres = [];
+  List<int> edades = [];
+  List<double> sueldos = [];
+  int n;
+
+  // Entrada Algoritmo
+  print('Ingrese el número de empleados:');
+  n = int.parse(stdin.readLineSync()!);
+
+  for (int i = 0; i < n; i++) {
+    print('Ingrese el nombre del empleado ${i + 1}:');
+    String nombre = stdin.readLineSync()!;
+    nombres.add(nombre);
+
+    print('Ingrese la edad del empleado ${i + 1}:');
+    int edad = int.parse(stdin.readLineSync()!);
+    edades.add(edad);
+
+    print('Ingrese el sueldo del empleado ${i + 1}:');
+    double sueldo = double.parse(stdin.readLineSync()!);
+    sueldos.add(sueldo);
+  }
+
+  // Proceso Algoritmo
+  double mayorSueldo = sueldos[0];
+  int indiceMayorSueldo = 0;
+
+  for (int i = 1; i < n; i++) {
+    if (sueldos[i] > mayorSueldo) {
+      mayorSueldo = sueldos[i];
+      indiceMayorSueldo = i;
+    }
+  }
+
+  // Salida Algoritmo
+  print('\nReporte de empleados:');
+  for (int i = 0; i < n; i++) {
+    print('Nombre: ${nombres[i]}, Edad: ${edades[i]}, Sueldo: \$${sueldos[i]}');
+  }
+
+  print('\nEmpleado con mayor sueldo:');
+  print('Nombre: ${nombres[indiceMayorSueldo]}, Edad: ${edades[indiceMayorSueldo]}, Sueldo: \$${sueldos[indiceMayorSueldo]}');
 }
